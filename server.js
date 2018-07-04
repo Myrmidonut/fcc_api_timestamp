5c2a7234584864191367aa861e8ccdfc5f6bc88c
Glitch (fcc-timestamp-microservice-fred) <none>
2018-07-04 21:50:01 +0000

    😱🚴 Updated with Glitch

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
-
+/*
 app.get("/api/timestamp/", function(req, res) {
   let date = new Date();
   res.json({"unix": date.getTime(), "utc" : date.toUTCString() });
@@ -35,7 +35,7 @@ app.get("/api/timestamp/:date_string", function(req, res) {
   console.log(date);
   res.json({"unix": date.getTime(), "utc" : date.toUTCString() });
 });
-
+*/
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
