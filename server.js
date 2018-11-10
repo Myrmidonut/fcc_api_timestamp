61c4ff8eed4812497ccaffc08c0c757a9ca9d0ed
Glitch (fcc-api-timestamp-microservice-fred) <none>
2018-11-10 00:28:48 +0000

    styling, readme

--- server.js
+++ server.js
@@ -14,17 +14,29 @@ app.get("/api/hello", (req, res) => {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/timestamp/", (req, res) => {
-  const date = new Date();
+app.get("/api/timestamp", (req, res) => {
+  let date;
+  
+  if (req.query.time === "" || !req.query.time) date = new Date();
+  else {
+    const date_number = Number(req.query.time);
+    let date_string = req.query.time;
+    
+    if (date_string == date_number) {
+      date_string = date_number;
+    }
+
+    date = new Date(date_string);
+  }
   
   res.json({
     "unix": date.getTime(),
-    "utc" : date.toUTCString()
+    "utc": date.toUTCString()
   });
 });
 
 app.get("/api/timestamp/:date_string", (req, res) => {
-  let date_number = Number(req.params.date_string);
+  const date_number = Number(req.params.date_string);
   let date_string = req.params.date_string;
   
   if (date_string == date_number) {
